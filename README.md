5a32eeba9e4ace0f9f086ddfa4c7074d8acec5ce
solphilos <trichosirius@gmail.com>
2022-02-27 20:48:23 -0500

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Crating-calculator
+
+The objective of this calculator is to produce the correct size of crate 
+after number of boxes are entered. 
+After entering number of boxes, followed by the terms "new", "old", or 
+"not boxed", the calculator should return the full dimensions of the crate, 
+along with other values such as amount of materials used, approximate weight, etc. 
+
---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.getElementsByClassName("clicky").onclick = function() {clicky()}
+
+   function clicky() {
+      
+      let answer = prompt('How many boxes?');
+   
+      switch (answer) { 
+        case '1':
+          alert('12x16')
+          break;
+        case '2':
+          alert('12x16')
+          break;
+        case '3':
+          alert('12x16')
+          break;
+      }
+   }

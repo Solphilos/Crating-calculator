66522e5c602173f96c391b7eb3f703977de7a3be
solphilos <trichosirius@gmail.com>
2022-02-27 20:53:19 -0500

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementsByClassName("clicky").onclick = function() {clicky()}
    
       switch (answer) { 
         case '1':
-          alert('12x16')
+          alert('crate size: 12x16  (wood used: 40\' of 2x4\'s)')
           break;
         case '2':
           alert('12x16')
@@ -15,5 +15,33 @@ document.getElementsByClassName("clicky").onclick = function() {clicky()}
         case '3':
           alert('12x16')
           break;
+          case '4':
+            alert('12x16')
+            break;
+          case '5':
+            alert('18x16')
+            break;
+          case '6':
+            alert('18x16')
+            break;
+          case '7':
+            alert('18x16')
+            break;
+          case '8':
+            alert('18x16')
+            break;
+          case '9':
+            alert('40x16')
+            break;
+          case '10':
+            alert('40x16')
+            break;
+         case '11':
+            alert('40x16')
+            break;
+          case '12':
+            alert('40x16')
+            break;
+        }
       }
-   }
+   

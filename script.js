1964fc189c0146b57dec206597de76204fa76802
solphilos <trichosirius@gmail.com>
2022-03-01 21:40:20 -0500

    style.css

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.getElementsByClassName("clicky").onclick = function() {clicky()}
 
    function clicky() {
       
-    // when button is clicked, text entered into text field is assigned to variable textField. //
-    let textField = document.getElementById("field").value; 
+    // obtains value of both text fields and asigns them to textField variable  //
+    let textField1 = document.getElementById("number-field").value; 
+    let textField2 = document.getElementById("type-field").value;
     
-    
-    
+  
      
-            switch (textField) { 
+            switch (textField1) { 
                case '1':
                 document.getElementById("result").innerHTML = '12x16';
                 break;
@@ -33,23 +33,24 @@ document.getElementsByClassName("clicky").onclick = function() {clicky()}
                   document.getElementById("result").innerHTML = '12x16';
                    break;
                  case '8':
-                  document.getElementById("result").innerHTML = '12x16';
+                  document.getElementById("result").innerHTML = '40x16';
                    break;
                  case '9':
-                  document.getElementById("result").innerHTML = '12x16';
+                  document.getElementById("result").innerHTML = '40x16';
                    break;
                  case '10':
-                  document.getElementById("result").innerHTML = '12x16';
+                  document.getElementById("result").innerHTML = '40x16';
                    break;
                 case '11':
-                  document.getElementById("result").innerHTML = '12x16';
+                  document.getElementById("result").innerHTML = '40x16';
                    break;
                  case '12':
-                  document.getElementById("result").innerHTML = '12x16';
+                  document.getElementById("result").innerHTML = '40x16';
+                  break;
                    
-                   
-             
             }
+             
+            
 
 
          }

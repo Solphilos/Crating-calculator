12edf40fb6b55847a0ccc87f5e4baf0ec27df416
solphilos <trichosirius@gmail.com>
2022-03-01 20:45:50 -0500

    index.html

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
 
+ 
 document.getElementsByClassName("clicky").onclick = function() {clicky()}
 
    function clicky() {
       
-      let answer = prompt('How many boxes?');
+    // when button is clicked, text entered into text field is assigned to variable textField. //
+    let textField = document.getElementById("field").value; 
+    
+    
+    
+     
+            switch (textField) { 
+               case '1':
+                document.getElementById("result").innerHTML = '12x16';
+                break;
+               case '2':
+                document.getElementById("result").innerHTML = '12x16';
+                 break;
+               case '3':
+                document.getElementById("result").innerHTML = '12x16';
+                 break;
+                 case '4':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '5':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '6':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '7':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '8':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '9':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '10':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                case '11':
+                  document.getElementById("result").innerHTML = '12x16';
+                   break;
+                 case '12':
+                  document.getElementById("result").innerHTML = '12x16';
+                   
+                   
+             
+            }
+
+
+         }
+      
    
-      switch (answer) { 
-        case '1':
-          alert('crate size: 12x16  (wood used: 40\' of 2x4\'s)')
-          break;
-        case '2':
-          alert('12x16')
-          break;
-        case '3':
-          alert('12x16')
-          break;
-          case '4':
-            alert('12x16')
-            break;
-          case '5':
-            alert('18x16')
-            break;
-          case '6':
-            alert('18x16')
-            break;
-          case '7':
-            alert('18x16')
-            break;
-          case '8':
-            alert('18x16')
-            break;
-          case '9':
-            alert('40x16')
-            break;
-          case '10':
-            alert('40x16')
-            break;
-         case '11':
-            alert('40x16')
-            break;
-          case '12':
-            alert('40x16')
-            break;
-        }
-      }
    

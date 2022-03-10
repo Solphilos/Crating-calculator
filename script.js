c8239378a1dabe5e5a328983dae2c9e761065e08
solphilos <trichosirius@gmail.com>
2022-03-10 17:12:10 -0500

    modified text field

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ document.getElementsByClassName("clicky").onclick = function() {clicky()}
    function clicky() {
 
       
-    // obtains value of text fields and asigns them to textField1 and textField2  variables //
-    let textField1 = document.getElementById("number-field").value; 
-    let textField2 = document.getElementById("type-field").value;
+    // obtains value of text fields and asigns them to Field1 and Field2  variables //
+    let newBoxes = document.getElementById("new").value; 
+    let oldBoxes = document.getElementById("old").value;
+   
+   
     
-if (textField2 === 'new') {
-
-    switch (textField1) { 
+ 
+  switch (newBoxes) { 
       case '1':
        document.getElementById("result").innerHTML = '12x16';
        break;
@@ -28,73 +29,34 @@ if (textField2 === 'new') {
          document.getElementById("result").innerHTML = '12x16';
           break;
         case '6':
-         document.getElementById("result").innerHTML = '12x16';
+          document.getElementById("result").innerHTML ='12x16';
           break;
-        case '7':
-         document.getElementById("result").innerHTML = '12x16';
-          break;
-        case '8':
-         document.getElementById("result").innerHTML = '40x16';
-          break;
-        case '9':
-         document.getElementById("result").innerHTML = '40x16';
-          break;
-        case '10':
-         document.getElementById("result").innerHTML = '40x16';
-          break;
-       case '11':
-         document.getElementById("result").innerHTML = '40x16';
-          break;
-        case '12':
-         document.getElementById("result").innerHTML = '40x16';
-         break;
-          
-   }
-     
-     
-}
-        else if (textField2 === 'old') {
-          switch (textField1) { 
-            case '1':
-             document.getElementById("result").innerHTML = 'lkjlkfd';
-             break;
-            case '2':
-             document.getElementById("result").innerHTML = '12x16';
-              break;
-            case '3':
-             document.getElementById("result").innerHTML = '12x16';
-              break;
-              case '4':
-               document.getElementById("result").innerHTML = '12x16';
-                break;
-              case '5':
-               document.getElementById("result").innerHTML = '12x16';
-                break;
-              case '6':
-               document.getElementById("result").innerHTML = '12x16';
-                break;
-              case '7':
-               document.getElementById("result").innerHTML = '12x16';
-                break;
-              case '8':
-               document.getElementById("result").innerHTML = '40x16';
-                break;
-              case '9':
-               document.getElementById("result").innerHTML = '40x16';
-                break;
-              case '10':
-               document.getElementById("result").innerHTML = '40x16';
-                break;
-             case '11':
-               document.getElementById("result").innerHTML = '40x16';
-                break;
-              case '12':
-               document.getElementById("result").innerHTML = '40x16';
-               break;
-         }
-        }     
+    }
             
+    switch (oldBoxes) {
+      case '1':
+       document.getElementById("result").innerHTML = '12x16';
+       break;
+      case '2':
+       document.getElementById("result").innerHTML = '12x16';
+        break;
+      case '3':
+       document.getElementById("result").innerHTML = '12x16';
+        break;
+        case '4':
+         document.getElementById("result").innerHTML = '12x16';
+          break;
+        case '5':
+         document.getElementById("result").innerHTML = '12x16';
+          break;
+        case '6':
+          document.getElementById("result").innerHTML ='12x16';
+          break;
+}
 
+    switch (newBoxes && oldBoxes) {
+      
+    }
 
          }
       

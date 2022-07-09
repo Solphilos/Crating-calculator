bce489a874c56a72f8dfea3a8e2e083e249d2db2
solphilos <trichosirius@gmail.com>
2022-07-08 21:21:31 -0400

    refactor code

--- script.js
+++ script.js
@@ -1,138 +1,137 @@
-resetValues();
+// tab navigation control 
+
+tabNav = (() => {
+  const input1 = document.getElementById('input1');
+  const input2 = document.getElementById('input2');
+  const homepage = document.getElementById('homepage');
+  const totalsWindow = document.getElementById('totals_window');
+  const unboxed = document.querySelector('.unboxed');
+  const boxed = document.querySelector('.boxed');
+  const home = document.querySelector('.home');
+  const totals = document.querySelector('.totals');
+  const tabs =  document.getElementById('tabs');
 
 
-function turnPage() {
-  document.getElementById('input1').style.display = "none";
-  document.getElementById('input2').style.display = "flex";
-  document.getElementById('homepage').style.display = "none"
-  document.getElementById('totals_window').style.display = "none";
-  document.querySelector('.unboxed').setAttribute('id', 'unboxed');
-  document.querySelector('.boxed').removeAttribute('id', 'boxed');
-  document.querySelector('.home').removeAttribute('id', 'home');
-  document.querySelector('.totals').removeAttribute('id', 'totals')
+  setDisplay = (element1, element2, element3, element4) => {        
+    element1.style.display = "none";  
+    element2.style.display = "none";
+    element3.style.display = "none";
+    element4.style.display = "flex";         
+  }
 
+  setAndRemove = (ele1, ele2, ele3, ele4) => {
+    let id1 = String(ele1);
+    let id2 = String(ele2);
+    let id3 = String(ele3);
+    let id4 = String(ele4);
+    ele1.removeAttribute('id', id1);
+    ele2.removeAttribute('id', id2);
+    ele3.removeAttribute('id', id3)
+    ele4.setAttribute('id', id4);
+  }
+
+
+
+  turnPage = () => {
+    setDisplay(input1, homepage, totalsWindow, input2)
+    setAndRemove(boxed, home, totals, unboxed)
+  }
+
+  turnPage2 = () => {
+    setDisplay(input2, homepage, totalsWindow, input1)
+    setAndRemove(unboxed, home, totals, boxed)
+  }
+
+  turnpageHome = () => {
+    setDisplay(input2, input1, totalsWindow, homepage)
+    setAndRemove(unboxed, boxed, totals, home)
+  }
+
+  turnpageResults = () => {
+    setDisplay(input2, input1, homepage, totalsWindow)
+    setAndRemove(unboxed, boxed, home, totals)
+  }
+
+
+  return {
+    turnPage,
+    turnPage2,
+    turnpageHome,
+    turnpageResults,
+  }
+
+})();
+
+// return all caculated values back to zero
+resetValues = (() => {
+  function reset() {
+    document.getElementById("height").innerHTML =  '0';
+    document.getElementById("width").innerHTML =  '0';
+    document.getElementById("length").innerHTML = '0';
+    document.getElementById("2x4").innerHTML =  '0';
+    document.getElementById("ply").innerHTML = '0';
+    document.getElementById("pallets").innerHTML = '0';
+    document.getElementById("amount").innerHTML = '0';
+  }
+
+  return {
+    reset,
+  }
+
+})();
+
+
+addListeners = (() => {
+  document.querySelector('.unboxed').addEventListener('click', tabNav.turnPage);
+  document.querySelector('.boxed').addEventListener('click', tabNav.turnPage2);
+  document.querySelector('.home').addEventListener('click', tabNav.turnpageHome);
+  document.querySelector('.totals').addEventListener('click', tabNav.turnpageResults)
+})();
+
+
+popResults = (length, width, height, boards, ply, crate_num) => {
+  document.getElementById("length").innerHTML = length;    
+  document.getElementById("width").innerHTML = width;     
+  document.getElementById("height").innerHTML = height;
+  document.getElementById("2x4").innerHTML = boards;
+  document.getElementById("ply").innerHTML = ply;
+  document.getElementById("amount").innerHTML = crate_num;
 }
-
-function turnPage2() {
-  document.getElementById('input2').style.display = "none"; 
-  document.getElementById('input1').style.display = "flex";
-  document.getElementById('homepage').style.display = "none"
-  document.getElementById('totals_window').style.display = "none";
-  document.querySelector('.unboxed').removeAttribute('id', 'unboxed');
-  document.querySelector('.boxed').setAttribute('id', 'boxed');
-  document.querySelector('.home').removeAttribute('id', 'home');
-  document.querySelector('.totals').removeAttribute('id', 'totals')
-}
-
-function turnpageHome() {
-  document.getElementById('input2').style.display = "none"; 
-  document.getElementById('input1').style.display = "none";
-  document.getElementById('homepage').style.display = "flex";
-  document.getElementById('totals_window').style.display = "none";
-  document.querySelector('.unboxed').removeAttribute('id', 'unboxed');
-  document.querySelector('.boxed').removeAttribute('id', 'boxed');
-  document.querySelector('.home').setAttribute('id', 'home');
-  document.querySelector('.totals').removeAttribute('id', 'totals')
-}
-
-function turnpageResults() {
-  document.getElementById('input2').style.display = "none"; 
-  document.getElementById('input1').style.display = "none";
-  document.getElementById('homepage').style.display = "none"
-  document.getElementById('totals_window').style.display = "flex";
-  document.querySelector('.unboxed').removeAttribute('id', 'unboxed');
-  document.querySelector('.boxed').removeAttribute('id', 'boxed');
-  document.querySelector('.home').removeAttribute('id', 'home');
-  document.querySelector('.totals').setAttribute('id', 'totals');
-  
-}
-
-
-function showInput() {
-  document.getElementById('totals_window').style.display = "none";      // hides result table
-  document.getElementById('homepage').style.display = "flex";          
-  document.getElementById('tabs').style.display = 'inline';            // displays tabs  
-  document.getElementById('input2').style.display = 'none';
-  document.querySelector('.unboxed').removeAttribute('id', 'unboxed');
-  
-}
-
-function resetValues() {
-  document.getElementById("height").innerHTML =  '0';
-  document.getElementById("width").innerHTML =  '0';
-  document.getElementById("length").innerHTML = '0';
-  document.getElementById("2x4").innerHTML =  '0';
-  document.getElementById("ply").innerHTML = '0';
-  document.getElementById("pallets").innerHTML = '0';
-  document.getElementById("crate_num").innerHTML = '0';
-}
-
-document.querySelector('.unboxed').addEventListener('click', turnPage);
-document.querySelector('.boxed').addEventListener('click', turnPage2);
-document.querySelector('.home').addEventListener('click', turnpageHome);
-document.querySelector('.totals').addEventListener('click', turnpageResults)
-
-
 
 
 function clicky() {
-  //document.getElementById('input1').style.display = 'none';      
-  //document.getElementById('input2').style.display = 'none';    
-  //document.getElementById('tabs').style.display = 'none';
-  //document.getElementById('homepage').style.display = 'none';
-  //document.getElementById('result-panel').style.display = "block";
-  turnpageResults();
+  let newBoxes = document.getElementById("new").value;       // gets value of text fields and asigns them to newBoxes and oldBoxes variables //
+  let oldBoxes = document.getElementById("old").value;
+  let select = document.getElementById('crate_type');
+  let minimal = select.options[select.selectedIndex].value;
+
+  tabNav.turnpageResults();
   document.querySelector('.reset').addEventListener('click', function() { 
-  // showInput()
-  resetValues();
-  document.querySelector('input[name="new"]').value = '';
-  document.querySelector('input[name="old"]').value = '';
-});
+    resetValues.reset()
+    document.querySelector('input[name="new"]').value = '';
+    document.querySelector('input[name="old"]').value = '';
+  });
    
 
-function popResults(length, width, height, boards, ply, crate_num) {
-    document.getElementById("length").innerHTML = length;    
-    document.getElementById("width").innerHTML = width;     
-    document.getElementById("height").innerHTML = height;
-    document.getElementById("2x4").innerHTML = boards;
-    document.getElementById("ply").innerHTML = ply;
-    document.getElementById("crate_num").innerHTML = crate_num;
-};
 
+  if (newBoxes && oldBoxes) {
+    // takes the value of both text fields and concatenates them as as string. example: 11 would mean 1 new, 1 old.  1112 would be 11 new, 12 old.
+    let reference = newBoxes + oldBoxes;
 
-
-
-
- // obtains value of text fields and asigns them to newBoxes and oldBoxes variables //
- let newBoxes = document.getElementById("new").value; 
- let oldBoxes = document.getElementById("old").value;
- 
- let select = document.getElementById('crate_type');
- let minimal = select.options[select.selectedIndex].value;
-
- 
-
-// if both box types are entered return appropriate values 
-
-if (newBoxes && oldBoxes) {
-// takes the value of both text fields and concatenates them as as string. example: 11 would mean 1 new, 1 old.  1112 would be 11 new, 12 old.
-let reference = newBoxes + oldBoxes;
-
-switch (reference) {
-  case '11':
-    document.getElementById("result1").innerHTML =  'Crate dimensions: 12" wide, 16\' long';
-    document.getElementById("result2").innerHTML = 'Wood used: 52\' of 2x4';
-    document.getElementById("result3").innerHTML = 'another value:';
-   break;
-   // this would mean 1 new box, 2 old
-   case '12': 
-   document.getElementById("result1").innerHTML = 'combo test 2';
-   break;
-   case '21':
-   document.getElementById("result1").innerHTML = 'combo test 3';
-   break;
+    switch (reference) {
+      case '11':
+        document.getElementById("result1").innerHTML =  'Crate dimensions: 12" wide, 16\' long';
+        document.getElementById("result2").innerHTML = 'Wood used: 52\' of 2x4';
+        document.getElementById("result3").innerHTML = 'another value:';
+      break;
+      case '12': 
+      document.getElementById("result1").innerHTML = 'combo test 2';
+      break;
+      case '21':
+      document.getElementById("result1").innerHTML = 'combo test 3';
+      break;
+    }
   }
- }
 
  
 
@@ -194,3 +193,5 @@ else if (oldBoxes) {
    
 }
 
+
+resetValues.reset()
